faa6c2021a0512109a48bf4be259a02e9e86a58e
Drew Smith <buddyJASmith@gmail.com>
2019-07-26 13:36:55 -0400

    new notes

---
+++ example.sql
@@ -0,0 +1,26 @@
+DECLARE
+    fname           employees.first_name%TYPE;
+    sal             employees.salary%TYPE;
+    eid             employees.employee_id%TYPE := &eid;
+    invalid_eid     EXCEPTION;  --USER DEFINED EXCEPTION
+BEGIN
+    IF  eid < 0   THEN
+        RAISE invalid_eid;
+    ELSE
+        SELECT first_name, salary 
+        INTO fname, sal
+        FROM employees
+        WHERE employee_id = eid;
+        dbms_output.put_line( fname || ' ' || sal);
+    END IF;
+
+    EXCEPTION
+      WHEN invalid_eid THEN
+            dbms_output.put_line('LESS THAN 0!');
+        WHEN NO_DATA_FOUND THEN
+            dbms_output.put_line('no such record exists');      -- IF NOT FOUND
+      
+        WHEN OTHERS THEN    
+            dbms_output.put_line('Error!');                 -- UNFORESEEN HANDLING1
+END;
+/
