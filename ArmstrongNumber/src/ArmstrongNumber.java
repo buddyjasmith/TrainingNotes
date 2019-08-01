0d01142f1d3d001e8bd932fc0ebe99f91e402281
Drew Smith <buddyjasmith@gmail.com>
2019-08-01 17:34:05 -0400

    --- Auto-Git Commit ---

--- ArmstrongNumber/src/ArmstrongNumber.java
+++ ArmstrongNumber/src/ArmstrongNumber.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
 public class ArmstrongNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		String possibleArm = keyboard.nextLine();
+		
+		int n = Integer.parseInt(possibleArm);
+		int original = n;
+		if(possibleArm.length() != 3) {
+			System.out.println("INVALID_INPUT");
+			return;
+		}
+		int mod;
+		int check=0;
+		while(n >0) {
+			check += Math.pow((n%10), possibleArm.length());
+			
+			n /= 10;
+		}
+		if(check == original) System.out.println("ARMSTRONG");
+		else System.out.println("NOT ARMSTRONG");
+		
+		
 
 	}
 
