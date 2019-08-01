46364ea1937c1afc9434e55846015a438fe245c5
Drew Smith <buddyjasmith@gmail.com>
2019-08-01 09:33:13 -0400

    --- Auto-Git Commit ---

--- SumOfPrimeNumbers/src/SumOfPrimeNumbers.java
+++ SumOfPrimeNumbers/src/SumOfPrimeNumbers.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class SumOfPrimeNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner keyboard = new Scanner(System.in);
+		int[] array = new int[2];
+		array[0] = keyboard.nextInt();
+		array[1] = keyboard.nextInt();
+		boolean flag = (array[0] < array[1]) ? true: false;
+		flag = (array[0] < 3 && array[0] > 1000) ? false:true;
+		flag = (array[1] < 3 && array[1] > 1000) ? false:true;
+		
+		
 	}
 
 }
