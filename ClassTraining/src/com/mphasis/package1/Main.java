f98486341b4cc04aaf9ad26ab4b5cb8023c1891f
Drew Smith <buddyJASmith@gmail.com>
2019-08-02 11:44:48 -0400

    update

---
+++ ClassTraining/src/com/mphasis/package1/Main.java
@@ -0,0 +1,11 @@
+package com.mphasis.package1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book book = new Book(110,"Java",300);
+		System.out.println(book);
+	}
+
+}
