cac80f0ccf35dc0b7ac3fd1297953521f61836cd
Drew Smith <buddyjasmith@gmail.com>
2019-08-05 13:24:54 -0400

    --- Auto-Git Commit ---

--- CollectionMapDemo/src/pack1/Main.java
+++ CollectionMapDemo/src/pack1/Main.java
@@ -1,5 +1,20 @@
 package pack1;
 
-public class Main {
+import java.util.HashMap;
 
+public class Main {
+	public static void main(String[] args) {
+		HashMap<Integer,String> hm = new HashMap<Integer, String>();
+		hm.put(1, "Drew");
+		hm.put(2,"Dang");
+		hm.put(3,"AAA");
+		hm.put(null,null);
+		System.out.println(hm.keySet());
+		System.out.println(hm.values());
+		System.out.println(hm.entrySet());
+		
+		for(Integer i : hm.keySet()) {
+			System.out.println("key "+ i+ " " +hm.get(i));
+		}
+	}
 }
